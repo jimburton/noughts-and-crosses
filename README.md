46ad306e387a201a93163f9b82d8fe7724c416c0
James Burton <j.burton@brighton.ac.uk>
2018-03-22 14:53:14 +0000

    WS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Playing with Spark
+
+http://sparkjava.com/tutorials/
+
+http://velocity.apache.org/engine/2.0/user-guide.html
+
+https://sparktutorials.github.io/2015/11/08/spark-websocket-chat.html
--- src/main/java/HelloWebSocket.java
+++ src/main/java/HelloWebSocket.java
@@ -1,8 +1,10 @@
 import static spark.Spark.init;
+import static spark.Spark.staticFileLocation;
 import static spark.Spark.webSocket;
 
 public class HelloWebSocket {
     public static void main(String[] args) {
+        staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
         webSocket("/echo", EchoWebSocket.class);
         init(); // Needed if you don't define any HTTP routes after your WebSocket routes
     }
